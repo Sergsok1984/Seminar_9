12d396c15b3547326e8cdeabf57e8ecac147c0d9
Сергей Соколов <sergsok1984@gmail.com>
2022-09-15 16:27:55 +0300

    add Exercise 64

---
+++ Exercise_64/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M={m}; N={n}");
+
+void NaturalNumber(int m, int n)
+{
+    if (n > m) 
+    {
+        for (int i = m; i <= n; i++)
+        Console.Write($"{i}, ");
+    }      
+    else
+    {
+        for (int i = n; i <= m; i++) 
+        Console.Write($"{i}, ");
+    }
+}
+NaturalNumber(m, n);
