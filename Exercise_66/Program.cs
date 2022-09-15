88f21d6d1dc189dcad1d755fc4bbfc9de63ed21d
Сергей Соколов <sergsok1984@gmail.com>
2022-09-15 17:19:05 +0300

    add Exercise 66

---
+++ Exercise_66/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M={m}; N={n}");
+
+int SumNumber(int m, int n)
+{
+    int sum = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        sum = m + SumNumber(m, n);
+        return sum;
+    }
+}
+
+void SumMN(int m, int n)
+{
+    Console.Write(SumNumber(m - 1, n));
+}
+SumMN(m, n);
