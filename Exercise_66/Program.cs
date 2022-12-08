2b1ca5ad08374a55669f1555cb07d19abab58ca2
Sergey Sokolov <sergsok1984@gmail.com>
2022-12-08 16:24:03 +0300

    add Task title

--- Exercise_66/Program.cs
+++ Exercise_66/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Введите первое число: ");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите первое число: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int n = Convert.ToInt32(Console.ReadLine());
--- Exercise_68/Program.cs
+++ Exercise_68/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Write("Введите первое число: ");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+Console.Write("Введите первое число: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int n = Convert.ToInt32(Console.ReadLine());
